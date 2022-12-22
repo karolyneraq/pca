4ba26944441128e8dd5ac4c90fe3e2cc88803e72
karolyneraq <karolyneraq@gmail.com>
2022-12-22 12:57:49 -0400

    Questão 6 atualizada

--- Terceira atividade/20222220006.c
+++ Terceira atividade/20222220006.c
@@ -5,7 +5,16 @@ int main(){
 	
 	for(i=1; i<=9; i++){
 		for(j=1; j<=10; j++){
-			printf("\n %d x %d = %d", i, j, i*j);	
+			printf("\n %d + %d = %d\t %d x %d = %d", i, j, i+j, i, j, i*j);	
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+	
+	for(i=1; i<=9; i++){
+		for(j=i; j<=i+10; j++){
+			printf("\n %d - %d = %d", j, i, j-i);	
 		}
 		printf("\n");
 	}
