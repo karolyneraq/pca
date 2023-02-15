d282a9f97ea80a1b476473519a5281c5d3d3b481
karolyneraq <karolyneraq@gmail.com>
2023-02-14 21:38:17 -0400

    Vetores

--- Vetores/20230124_006.c
+++ Vetores/20230124_006.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
-#define max 100
+
+
+void selection(int array[100], int num){
+	int i, j, aux, achou=0;
+    int pos, menor;
+    
+	for (i = 0; i < num; i++)   {
+		
+        menor = array[i];
+        
+        for (j = i+1; j < num; j++)   {
+        	
+            if (array[j] < menor)   {
+                menor = array[j];
+                pos = j;
+                aux = array[i];
+                achou = 1;
+            }
+            
+        }
+        
+        if(achou==1){
+        	
+            array[i] = menor;
+            array[pos]= aux;
+        }
+    }
+    
+    printf("\nArray Ordenado: ");
+    
+    for(i=0; i<num; i++){
+    	
+        printf("%d ", array[i]);
+        
+    }
+}
+
 
 int main()  {
-    int array[max], sorted_array[max], i, j, num;
-    int aux, achou=0;
-    int pos, menor;
+    int array[100], num, i;
 
     printf("Quantos elementos o vetor tera?: ");
     scanf("%d", &num);
@@ -13,25 +47,8 @@ int main()  {
         printf("Digite um numero: ");
         scanf("%d", &array[i]);
     }
-    for (i = 0; i < num; i++)   {
-        menor = array[i];
-        for (j = i+1; j < num; j++)   {
-            if (array[j] < menor)   {
-                menor = array[j];
-                pos = j;
-                aux = array[i];
-                achou = 1;
-            }
-        }
-        if(achou==1){
-            array[i] = menor;
-            array[pos]= aux;
-        }
-    }
-    printf("\n");
-    for(i=0; i<num; i++){
-        printf("%d ", array[i]);
-    }
+    
+    selection(array, num);
 
     return 0;
 }
